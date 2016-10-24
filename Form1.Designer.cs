a2507fcc1b690a899d9476363d188c4f60213fc8
rutgersmit <rutger.smit@gmail.com>
2016-10-24 09:00:08 +0200

    Initial commit
    \m/

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace MediaControl
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.pauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.muteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.contextMenuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "Audio Control";
+			this.notifyIcon1.Visible = true;
+			this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.pauseToolStripMenuItem,
+            this.muteToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem1});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(153, 120);
+			// 
+			// pauseToolStripMenuItem
+			// 
+			this.pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
+			this.pauseToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.pauseToolStripMenuItem.Text = "Pause";
+			this.pauseToolStripMenuItem.Click += new System.EventHandler(this.pauseToolStripMenuItem_Click);
+			// 
+			// muteToolStripMenuItem
+			// 
+			this.muteToolStripMenuItem.Name = "muteToolStripMenuItem";
+			this.muteToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.muteToolStripMenuItem.Text = "Mute";
+			this.muteToolStripMenuItem.Click += new System.EventHandler(this.muteToolStripMenuItem_Click);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+			// 
+			// exitToolStripMenuItem1
+			// 
+			this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+			this.exitToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+			this.exitToolStripMenuItem1.Text = "Exit";
+			this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+			// 
+			// toolStripMenuItem1
+			// 
+			this.toolStripMenuItem1.CheckOnClick = true;
+			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+			this.toolStripMenuItem1.Text = "Run at startup";
+			this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.ShowInTaskbar = false;
+			this.Text = "Form1";
+			this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+						this.contextMenuStrip1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripMenuItem muteToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace MediaControl
+{
+	public partial class Form1 : Form
+	{
+		private const int KEYEVENTF_EXTENDEDKEY = 1;
+		private const int VK_MEDIA_PLAY_PAUSE = 0xB3;
+		private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+		private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+		private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+		private const int WM_APPCOMMAND = 0x319;
+
+		readonly KeyboardHook _hook = new KeyboardHook();
+		readonly RegistryKey _rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			if (_rk.GetValue("Audio Control") == null)
+			{
+				// The value doesn't exist, the application is not set to run at startup
+				toolStripMenuItem1.Checked = false;
+			}
+			else
+			{
+				// The value exists, the application is set to run at startup
+				toolStripMenuItem1.Checked = true;
+			}
+
+			_hook.KeyPressed += new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
+			_hook.RegisterHotKey(MediaControl.ModifierKeys.Win, Keys.NumPad1);
+			_hook.RegisterHotKey(MediaControl.ModifierKeys.Win, Keys.NumPad2);
+			_hook.RegisterHotKey(MediaControl.ModifierKeys.Win, Keys.NumPad3);
+			_hook.RegisterHotKey(MediaControl.ModifierKeys.Win, Keys.NumPad5);
+		}
+
+		void hook_KeyPressed(object sender, KeyPressedEventArgs e)
+		{
+			switch (e.Key)
+			{
+				case Keys.NumPad1:
+					VolDown();
+					break;
+				case Keys.NumPad2:
+					Mute();
+					break;
+				case Keys.NumPad3:
+					VolUp();
+					break;
+				case Keys.NumPad5:
+					Pause(); // Or play
+					break;
+			}
+		}
+
+		[DllImport("user32.dll", SetLastError = true)]
+		public static extern void keybd_event(byte virtualKey, byte scanCode, uint flags, IntPtr extraInfo);
+		
+		[DllImport("user32.dll")]
+		public static extern IntPtr SendMessageW(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+
+		private void Pause()
+		{
+			keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENDEDKEY, IntPtr.Zero);
+		}
+		
+		private void VolDown()
+		{
+			SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)APPCOMMAND_VOLUME_DOWN);
+		}
+
+		private void VolUp()
+		{
+			SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)APPCOMMAND_VOLUME_UP);
+		}
+
+		private void Mute()
+		{
+			SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)APPCOMMAND_VOLUME_MUTE);
+		}
+
+
+		private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void pauseToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Pause();
+		}
+
+		private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+		{
+			Mute();
+		}
+
+		private void muteToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Mute();
+		}
+
+		private void toolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			if (toolStripMenuItem1.Checked)
+			{
+				// Add the value in the registry so that the application runs at startup
+				_rk.SetValue("Audio Control", Application.ExecutablePath);
+			}
+			else
+			{
+				// Remove the value from the registry so that the application doesn't start
+				_rk.DeleteValue("Audio Control", false);
+			}
+		}
+	}
+}
